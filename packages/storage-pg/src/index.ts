fcc495f919252651bc572cdcef684afa8fb05fd8
Jonas Strassel <info@jonas-strassel.de>
2022-10-03 14:54:13 +0200

    feat: add module for postgres storage

---
+++ packages/storage-pg/src/index.ts
@@ -0,0 +1,26 @@
+import type { CachedItem, Storage } from "@boredland/node-ts-cache"
+
+export class PgStorage implements Storage {
+    constructor(private tableName: string, private rawQuery: (query: string) => Promise<{ key: string, value: CachedItem }[] | undefined>) { }
+
+    async clear(): Promise<void> {
+        await this.rawQuery(`TRUNCATE TABLE ${this.tableName};`)
+    }
+
+    async getItem(key: string): Promise<CachedItem | undefined> {
+        const queryResult = await this.rawQuery(`SELECT key, value FROM ${this.tableName} WHERE key = '${key}'`)
+        const result = queryResult;
+
+        if (!result || result.length === 0) return undefined;
+
+        return { ...result[0].value }
+    }
+
+    async setItem(key: string, value: CachedItem): Promise<void> {
+        await this.rawQuery(`INSERT INTO ${this.tableName} (key, value) VALUES ('${key}', '${JSON.stringify(value)}') ON CONFLICT (key) DO UPDATE SET value = '${JSON.stringify(value)}'`)
+    }
+
+    async removeItem(key: any): Promise<void> {
+        await this.rawQuery(`DELETE FROM ${this.tableName} WHERE key = '${key}'`)
+    }
+}
