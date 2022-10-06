4ab6c3e42fd55948b765a375c6bcb9e8f4554cf4
Jonas Strassel <info@jonas-strassel.de>
2022-10-06 23:33:22 +0200

    feat: use prepared statements for pg-storage
    while it isn't all too likely that users a passing in values from their
    end-users, this is just a security baseline.

--- packages/storage-pg/src/index.ts
+++ packages/storage-pg/src/index.ts
@@ -8,17 +8,19 @@ export class PgStorage implements Storage {
   constructor(
     private tableName: string,
     private rawQuery: (
-      query: string
+      query: string,
+      values?: unknown[]
     ) => Promise<{ key: string; value: CachedItem }[] | undefined>
   ) {}
 
   async clear(): Promise<void> {
-    await this.rawQuery(`TRUNCATE TABLE ${this.tableName};`);
+    await this.rawQuery(`TRUNCATE TABLE ${this.tableName}`);
   }
 
   async getItem(key: string): Promise<CachedItem | undefined> {
     const result = await this.rawQuery(
-      `SELECT key, value FROM ${this.tableName} WHERE key = '${key}'`
+      `SELECT key, value FROM ${this.tableName} WHERE key = $1`,
+      [key]
     );
 
     if (!result || result.length === 0) return undefined;
@@ -28,15 +30,12 @@ export class PgStorage implements Storage {
 
   async setItem(key: string, value: CachedItem): Promise<void> {
     await this.rawQuery(
-      `INSERT INTO ${
-        this.tableName
-      } (key, value) VALUES ('${key}', '${JSON.stringify(
-        value
-      )}') ON CONFLICT (key) DO UPDATE SET value = '${JSON.stringify(value)}'`
+      `INSERT INTO ${this.tableName} (key, value) VALUES ($1, $2) ON CONFLICT (key) DO UPDATE SET value = $2`,
+      [key, JSON.stringify(value)]
     );
   }
 
   async removeItem(key: string): Promise<void> {
-    await this.rawQuery(`DELETE FROM ${this.tableName} WHERE key = '${key}'`);
+    await this.rawQuery(`DELETE FROM ${this.tableName} WHERE key = $1`, [key]);
   }
 }
